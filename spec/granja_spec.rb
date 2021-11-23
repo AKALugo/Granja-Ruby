b026b186e86ec763d89408b045d50660a7d79625
Alejandro <alu0101329185@ull.edu.es>
2021-11-23 17:57:05 +0000

    Eliminación de la espectativa por defecto

--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Granja do
   it "has a version number" do
     expect(Granja::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
