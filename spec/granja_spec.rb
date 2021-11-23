59afe4f0fce5fb6e103c102b8b3ee3306f1531c2
Alejandro <alu0101329185@ull.edu.es>
2021-11-23 20:54:18 +0000

    Fallo: Falta el atributo condición de vida animal

--- spec/granja_spec.rb
+++ spec/granja_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe Granja do
     it "Tiene una clase para almacenar las funcionalidades" do
       expect(Granja::Funcion.new()).not_to eq(nil)
     end
+    it "Tiene un atributo para las condiciones de vida de los animales" do
+      expect(Granja::Funcion.new("Muy buena").condicion_de_vida_animal).to eq("Muy buena")
+      expect(Granja::Funcion.new("Mala").condicion_de_vida_animal).to eq("Mala")
+      expect(Granja::Funcion.new(10).condicion_de_vida_animal).to eq(10)
+      expect(Granja::Funcion.new(5.4).condicion_de_vida_animal).to eq(5.4)
+    end
   end
 end
