80f388f5ff8ce99bf5aa3eedf9918461906497bc
Alejandro <alu0101329185@ull.edu.es>
2021-11-24 17:26:15 +0000

    Documentacion con Yard

--- lib/granja/datos.rb
+++ lib/granja/datos.rb
@@ -1,8 +1,11 @@
 
+# Clase que se encarga de guardar los datos de la granja.
 class Datos
-
+  
+  # @overload Getters.
   attr_reader :identificacion, :nombre, :tipo, :descripcion
 
+  # Método que inicializa las variables de instancia.
   def initialize (iden = "", nom = "", tip = "", desc = "")
 
     @identificacion = iden
@@ -11,6 +14,8 @@ class Datos
     @descripcion = desc
   end
 
+  # Metodo que convierte a String.
+  # @return [String]
   def to_s
 
     "La identificacion de la clase es: #{@identificacion}\nEl nombre de la granja es: #{@nombre}\nEl tipo de granja es: #{@tipo}\nLa descripcion de la granja es: #{@descripcion}"
--- lib/granja/funcion.rb
+++ lib/granja/funcion.rb
@@ -1,8 +1,10 @@
-
+# Clase que almacena las funciones de la granja.
 class Funcion
 
+  # @overload Getters.
   attr_reader :condicion_de_vida_animal, :cuidado_animal, :reproduccion_animal
 
+  # Método que inicializa las variables instancia.
   def initialize (cond_vid = "", cui_ani = "", repr_ani = "")
 
     @condicion_de_vida_animal = cond_vid
@@ -10,6 +12,8 @@ class Funcion
     @reproduccion_animal = repr_ani
   end
 
+  # Método que convierta a String
+  # @return [String]
   def to_s
 
     "La calidad de vida de los animales es: #{@condicion_de_vida_animal}\nLa calidad de los cuidados animales son: #{@cuidado_animal}\nLa calidad de la reproduccion es: #{@reproduccion_animal}"
