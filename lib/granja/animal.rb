af823748e95c8b9f118a53fbba77dab6f8cf5795
Alejandro <alu0101329185@ull.edu.es>
2021-12-05 17:59:57 +0000

    Atributos

--- lib/granja/animal.rb
+++ lib/granja/animal.rb
@@ -1,7 +1,12 @@
 
 class Animal
 
-    def initialize (iden = 0, e = 0, sex = "", pe = 0)
+    attr_reader :identificador, :edad, :sexo, :peso
 
+    def initialize (iden = 0, e = 0, sex = "", pe = 0)
+        @identificador = iden
+        @edad = e
+        @sexo = sex
+        @peso = pe
     end
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -13,6 +13,31 @@ RSpec.describe Granja do
             it "Tiene una clase para representar animales" do
                 expect(Animal.new()).not_to eq(nil)
             end
+
+            it "Tiene un atributo para identificar al animal" do
+                expect(@uno.identificador).to eq(1)
+                expect(@dos.identificador).to eq(2)
+                expect(@tres.identificador).to eq(3)
+                expect(@cuatro.identificador).to eq(50)
+            end
+            it "Tiene un atributo con la edad del animal en dıas" do
+                expect(@uno.edad).to eq(400)
+                expect(@dos.edad).to eq(10)
+                expect(@tres.edad).to eq(333)
+                expect(@cuatro.edad).to eq(1001)
+            end
+            it "Tiene un atributo con el sexo del animal" do
+                expect(@uno.sexo).to eq("macho")
+                expect(@dos.sexo).to eq("hembra")
+                expect(@tres.sexo).to eq("macho")
+                expect(@cuatro.sexo).to eq("hembra")
+            end
+            it "Tiene un atributo con el peso del animal en gramos" do
+                expect(@uno.peso).to eq(9000)
+                expect(@dos.peso).to eq(2000)
+                expect(@tres.peso).to eq(5033)
+                expect(@cuatro.peso).to eq(8888)
+            end
         end
 
 
