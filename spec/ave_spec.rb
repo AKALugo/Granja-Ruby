c12b3e33f3e6f88e8be69302111ee3014d2912a2
Alejandro <alu0101329185@ull.edu.es>
2021-12-07 16:56:08 +0000

    Cambio en las expectatibas de la jerarquía

--- spec/ave_spec.rb
+++ spec/ave_spec.rb
@@ -42,40 +42,40 @@ RSpec.describe Granja do
 
         context "Herencia de la clase Ave" do
             it "Se espera que una instancia de la clase Ave sea un Ave" do
-                expect(@uno.is_a? Ave).to eq(true)
-                expect(@dos.is_a? Ave).to eq(true)
+                expect(@uno).to be_a_kind_of(Ave)
+                expect(@dos).to be_a_kind_of(Ave)
                 expect(@tres.is_a? Ave).to eq(true)
                 expect(@cuatro.is_a? Ave).to eq(true)
             end
             it "Se espera que una instancia de la clase Ave sea un Animal" do
-                expect(@uno.is_a? Animal).to eq(true)
-                expect(@dos.is_a? Animal).to eq(true)
+                expect(@uno).to be_a_kind_of(Animal)
+                expect(@dos).to be_a_kind_of(Animal)
                 expect(@tres.is_a? Animal).to eq(true)
                 expect(@cuatro.is_a? Animal).to eq(true)
             end
             it "Se espera que una instancia de la clase Ave sea un objeto (Object)" do
-                expect(@uno.is_a? Object).to eq(true)
-                expect(@dos.is_a? Object).to eq(true)
+                expect(@uno).to be_a_kind_of(Object)
+                expect(@dos).to be_a_kind_of(Object)
                 expect(@tres.is_a? Object).to eq(true)
                 expect(@cuatro.is_a? Object).to eq(true)
             end
             it "Se espera que una instancia de la clase Ave sea un objeto basico (BasicObject)" do
-                expect(@uno.is_a? BasicObject).to eq(true)
-                expect(@dos.is_a? BasicObject).to eq(true)
+                expect(@uno).to be_a_kind_of(BasicObject)
+                expect(@dos).to be_a_kind_of(BasicObject)
                 expect(@tres.is_a? BasicObject).to eq(true)
                 expect(@cuatro.is_a? BasicObject).to eq(true)
             end
             it "No se espera que una instancia de la clase Ave sea una cadena (String)" do
-                expect(@uno.is_a? String).to eq(false)
-                expect(@dos.is_a? String).to eq(false)
+                expect(@uno).not_to be_a_kind_of(String)
+                expect(@dos).not_to be_a_kind_of(String)
                 expect(@tres.is_a? String).to eq(false)
                 expect(@cuatro.is_a? String).to eq(false)
             end
             it "No se espera que una instancia de la clase Ave sea un n´umero (Numeric)" do
-                expect(@uno.is_a? Numeric).to eq(false)
-                expect(@dos.is_a? Numeric).to eq(false)
+                expect(@uno).not_to be_a_kind_of(Numeric)
+                expect(@dos).not_to be_a_kind_of(Numeric)
                 expect(@tres.is_a? Numeric).to eq(false)
-                expect(@cuatro.is_a? Numeric).to eq(false)  
+                expect(@cuatro.is_a? Numeric).to eq(false)   
             end
         end
 
